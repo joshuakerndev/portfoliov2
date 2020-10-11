2ab638389987e2b18b2855b1eb03f737766734f4
Joshua Kern <joshuakerndev@gmail.com>
2020-10-10 17:27:59 -0700

    Finish up contact page styling, links, info, icons

--- src/components/ContactPage/ContactPage.jsx
+++ src/components/ContactPage/ContactPage.jsx
@@ -8,6 +8,38 @@ const ContactPage = () => {
             <div className="ContactHeader">
                 <h1>Contact</h1>
             </div>
+
+            <div className="ContactInfo">
+                <span>Email:</span>
+                <p>joshuakerndev@gmail.com</p>
+                <span>Phone:</span>
+                <p>(626)342-5550</p>
+                <div className="SocialMediaLinks">
+                    <span>
+                        <a 
+                            href="https://github.com/joshuakerndev"
+                            target="_blank"
+                        >
+                            <img 
+                                src={require("../../imgs/GitHub-Mark-Light-32px.png")} 
+                            />
+                        </a>
+                    </span>
+                    <span>
+                        <a 
+                            href="https://www.linkedin.com/in/joshua-kern-99bb031b4/"
+                            target="_blank"
+                        >
+                            <img 
+                                className="LinkedInIcon" 
+                                src={require("../../imgs/Linked-In-White.png")} 
+                            />                        
+                        </a>
+                    </span>
+                </div>
+            </div>
+
+            <div className="ContactFooter"></div>
         </div>
     )
 }
